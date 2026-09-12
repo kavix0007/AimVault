78a9121ed971b28e4b932b4864fa525ee3fe5db5
kavish <kavishture@gmail.com>
2026-09-12 18:13:50 +0530

    Add files via upload

--- assets/data/crosshairs.js
+++ assets/data/crosshairs.js
@@ -1,12 +1,23 @@
-const crosshairs = [
-  {name:'Green Precision',category:'Other',player:'',isPro:false,image:'https://cdn.phototourl.com/free/2026-09-06-f815dbc4-0644-4cf6-81e0-987c67df4655.png',code:'0;P;c;1;h;0;f;0;0l;2;0o;1;0a;1;0f;0;1b;0',added:'2026-09-06'},
+/*
+  AimVault shared crosshair database.
+
+  This is the SAME data that lives inline in index.html. It is duplicated
+  here (not invented) so that the new SEO pages (category pages, pro pages,
+  individual crosshair pages) can render real, existing records without a
+  build step.
+
+  If you add/edit a crosshair on the homepage, mirror the same change here
+  so both stay in sync.
+
+  ADDING A PRO CROSSHAIR: set isPro:true — it will show up in the Pro
+  filter automatically.
+*/
+window.AIMVAULT_CROSSHAIRS = [
+  {name:'Green Precision',category:'Other',player:'',isPro:false,image:'https://cdn.phototourl.com/free/2026-09-06-f815dbc4-0644-4cf6-81e0-987c67df4655.png',code:'0;P;c;1;h;0;f;0;0l;2;0o;1;0a;1;0f;0;1b;0',copies:0,added:'2026-09-06'},
   {name:'Shuriken',category:'Other',player:'',isPro:false,image:'https://cdn.phototourl.com/free/2026-09-06-ef24e720-2551-4c18-be64-45cca27b061b.png',code:'0;P;c;7;h;0;f;0;0l;4;0o;2;0a;1;0f;0;1t;8;1l;1;1o;1;1a;1;1m;0;1f;0',copies:0,added:'2026-09-06'},
   {name:'Sakura',category:'Fun',player:'',isPro:false,image:'https://cdn.phototourl.com/free/2026-09-06-3ecdacb9-c928-43bd-8663-2fbf24a24d96.png',code:'0;P;c;8;u;F97AC0FF;o;0.1;b;1;m;1;0t;3;0l;3;0o;1;0a;1;0f;0;1t;1;1l;5;1o;0;1a;0.8;1m;0;1e;0.1',copies:0,added:'2026-09-06'},
   {name:'Pink X',category:'Fun',player:'',isPro:false,image:'https://cdn.phototourl.com/free/2026-09-06-7ff17a57-5a9d-44fd-8bcd-83303f1df32e.png',code:'0;s;1;P;c;8;u;F8B5E8FF;h;0;d;1;b;1;f;0;0t;6;0l;1;0o;1;0a;1;0f;0;1v;0;1g;1;1o;6;1a;1;1m;0;1f;0;S;c;0;s;0.75;o;1',copies:0,added:'2026-09-06'},
   {name:'Star of David',category:'Fun',player:'',isPro:false,image:'https://cdn.phototourl.com/free/2026-09-09-5840df0b-3408-47ec-bda3-06295e3d3ffa.png',code:'0;P;c;8;u;000000FF;h;0;b;1;0t;8;0l;0;0v;1;0g;1;0o;1;0a;1;0f;0;1v;3;1g;1;1o;1;1a;1;1m;0;1f;0',copies:0,added:'2026-09-06'},
-  
-  /* Example pro entries — add more here with isPro:true */
-  
   {name:'TenZ',category:'pro',player:'TenZ',isPro:true,image:'https://cdn.phototourl.com/free/2026-08-31-8beddda9-408d-4ff3-b5d0-b315f5d99cf5.png',code:'0;s;1;P;c;5;h;0;m;1;0l;4;0o;2;0a;1;0f;0;1b;0;S;c;4;o;1',copies:0,added:'2026-09-06'},
   {name:'small dot',category:'pro',player:'',isPro:true,image:'https://cdn.phototourl.com/free/2026-09-02-fe0e12b1-537a-4a3e-8d9d-f0c54e6c2863.png',code:'0;P;d;1;f;0;0t;4;0l;1;0o;0;0a;1;0f;0;1b;0',copies:0,added:'2026-09-06'},
   {name:'Aim bot',category:'pro',player:'',isPro:true,image:'https://cdn.phototourl.com/free/2026-09-03-4de9ed7e-e591-4c5f-aaba-813fc6de20e0.png',code:'0;s;1;P;o;0.1;f;0;s;0;0t;1;0l;2;0o;1;0a;1;0f;0;1b;0',copies:0,added:'2026-09-06'},
@@ -14,6 +25,5 @@ const crosshairs = [
   {name:'Cyan Dot',category:'pro',player:'',isPro:true,image:'https://cdn.phototourl.com/free/2026-09-06-619f8760-3645-4877-a755-af19560355a3.png',code:'0;P;c;5;o;1;d;1;0l;1;0o;1;0a;1;0f;0;1b;0',copies:0,added:'2026-09-06'},
   {name:'Classic White',category:'other',player:'',isPro:false,image:'https://cdn.phototourl.com/free/2026-09-06-6aa7b586-373f-4d9e-9515-809270a5ca6f.png',code:'0;P;h;0;f;0;0l;4;0o;0;0a;1;0f;0;1b;0',copies:0,added:'2026-09-06'},
   {name:'underrated cyan',category:'Teams',player:'',isPro:false,image:'https://cdn.phototourl.com/free/2026-09-09-618a7877-6e89-40ec-97b4-d9c8da59b4f6.png',code:'0;s;1;P;c;5;o;0;f;0;0l;2;0v;2;0g;1;0o;1;0a;1;0f;0;1b;0;S;o;0.8',copies:0,added:'2026-09-06'},
-  {name:'Tiny White Dot',category:'pro',player:'',isPro:true,image:'https://cdn.phototourl.com/free/2026-09-06-95345a83-4fc7-4920-a5a0-83924ae44198.png',code:'0;P;h;0;d;1;0b;0;1b;0',copies:0,added:'2026-09-06'},
-
+  {name:'Tiny White Dot',category:'pro',player:'',isPro:true,image:'https://cdn.phototourl.com/free/2026-09-06-95345a83-4fc7-4920-a5a0-83924ae44198.png',code:'0;P;h;0;d;1;0b;0;1b;0',copies:0,added:'2026-09-06'}
 ];
